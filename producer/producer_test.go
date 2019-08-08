03482bb64bf0b4146e0a683fa715f44726b25c88
Dinesh Kumar <dinesh.kumar@go-jek.com>
2019-08-08 16:20:08 +0530

    Fixing lint, golangci issue, move mock to test file

--- producer/producer_test.go
+++ producer/producer_test.go
@@ -5,9 +5,6 @@ import (
 	"sync"
 	"testing"
 
-	"sync"
-	"testing"
-
 	"github.com/gojekfarm/kafqa/config"
 	"github.com/gojekfarm/kafqa/logger"
 	"github.com/stretchr/testify/assert"
@@ -97,3 +94,20 @@ func (m *msgCreatorMock) NewBytes() ([]byte, error) {
 	args := m.Called()
 	return args.Get(0).([]byte), args.Error(1)
 }
+
+type kafkaProducerMock struct{ mock.Mock }
+
+func (m *kafkaProducerMock) Produce(msg *kafka.Message, events chan kafka.Event) error {
+	args := m.Called(msg, events)
+	return args.Error(0)
+}
+
+func (m *kafkaProducerMock) Flush(t int) int {
+	return m.Called(t).Int(0)
+}
+
+func (m *kafkaProducerMock) Events() chan kafka.Event {
+	return m.Called().Get(0).(chan kafka.Event)
+}
+
+func (m *kafkaProducerMock) Close() { m.Called() }
